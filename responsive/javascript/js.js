2c3150895f9cbadbaecb951853fc94b6049e58d4
kimmin8 <nmnmy0201@gmail.com>
2025-07-16 14:28:34 +0900

    responsive

---
+++ responsive/javascript/js.js
@@ -0,0 +1,20 @@
+// 한줄주석
+// 제이쿼리 선언(연결)
+
+// 화면을 열었을때, 다음과 같은 일들이 일어나라
+
+$(document).ready(function(){
+
+// 실행문 작성
+//클래스 toggle을 클릭했을 때, 클래스 gnb가 슬라이드toggle되어라.
+
+$('.toggle').click(function(){
+
+
+$('.gnb').slideToggle()
+
+
+})
+
+
+})
